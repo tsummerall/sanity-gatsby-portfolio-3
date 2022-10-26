b3952d1e63b8d3f79eaf500ad0c5cec714c740a7
Thomas Summerall <thomassummerall@Thomass-MacBook-Pro.local>
2022-10-26 14:59:29 -0400

    Added gatsyby-omni-font-loader

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,26 +1,39 @@
 // Load variables from `.env` as soon as possible
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV || 'development'}`
-})
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV || "development"}`,
+});
 
-const clientConfig = require('./client-config')
-const token = process.env.SANITY_READ_TOKEN
+const clientConfig = require("./client-config");
+const token = process.env.SANITY_READ_TOKEN;
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   plugins: [
-    'gatsby-plugin-postcss',
-    'gatsby-plugin-image',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-source-sanity',
+      resolve: "gatsby-source-sanity",
       options: {
         ...clientConfig.sanity,
         token,
         watchMode: !isProd,
-        overlayDrafts: !isProd && token
-      }
-    }
-  ]
-}
+        overlayDrafts: !isProd && token,
+      },
+    },
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `Fasthand`,
+            file: `https://fonts.googleapis.com/css2?family=Raleway&display=swap`,
+          },
+        ],
+      },
+    },
+  ],
+};
